d83136e161de23e0c7ee57240e8ddd879a80c95b
H3XORSauvage <mikefhas@gmail.com>
2024-07-13 14:02:52 +0200

    Add files via upload

---
+++ DLU.py
@@ -0,0 +1,89 @@
+import os
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit, QFileDialog
+from PyQt5.QtGui import QColor
+
+class RechercheDBApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.dossier_parent = ''
+
+    def initUI(self):
+        self.setWindowTitle('Tool By Saucisson')
+        self.setGeometry(100, 100, 800, 600)
+        self.setStyleSheet("background-color: #1a2b3c; color: #333333;")
+
+        layout = QVBoxLayout()
+
+        input_layout = QHBoxLayout()
+
+        self.batabase_input = QLineEdit(self)
+        self.batabase_input.setPlaceholderText("Entrez la db à rechercher")
+        self.batabase_input.setStyleSheet("background-color: #ffffff; color: #333333;")
+
+
+        self.dossier_button = QPushButton("Choisir Dossier (Db/database)", self)
+        self.dossier_button.setStyleSheet("background-color: #4CAF50; color: white;")
+        self.dossier_button.clicked.connect(self.choisir_dossier)
+
+
+        self.rechercher_button = QPushButton("Rechercher", self)
+        self.rechercher_button.setStyleSheet("background-color: #008CBA; color: white;")
+        self.rechercher_button.clicked.connect(self.lancer_recherche)
+
+
+        input_layout.addWidget(QLabel("Data:"))
+        input_layout.addWidget(self.batabase_input)
+        input_layout.addWidget(self.dossier_button)
+        input_layout.addWidget(self.rechercher_button)
+
+        #résulta
+        self.resultats_text = QTextEdit(self)
+        self.resultats_text.setReadOnly(True)
+        self.resultats_text.setStyleSheet("background-color: #1a1d21; color: #ffffff;")
+
+        layout.addLayout(input_layout)
+        layout.addWidget(self.resultats_text)
+
+        self.setLayout(layout)
+
+    def choisir_dossier(self):
+        self.dossier_parent = QFileDialog.getExistingDirectory(self, "Choisir le dossier principal")
+
+    def lancer_recherche(self):
+        batabase = self.batabase_input.text()
+        if self.dossier_parent and batabase:
+            self.resultats_text.clear()
+            resultats = self.dossiersDb_recherche(self.dossier_parent, batabase)
+            if resultats:
+                for nom_fichier, ligne_numero, ligne in resultats:
+                    self.resultats_text.append(f"<span style='color:green'><b>[NEW]</b></span> {nom_fichier}, ligne {ligne_numero}: {ligne.strip()}")
+                    self.resultats_text.append(f"    {ligne}\n \n")
+            else:
+                self.resultats_text.setText(f"Aucune Db trouvée pour '<b>{batabase}</b>'.")
+        else:
+            self.resultats_text.setText("Veuillez sélectionner un dossier et entrer un Id,Ip,Steamid....")
+
+    def recherche_DB(self, nom_fichier, batabase):
+        resultats = []
+        with open(nom_fichier, 'r', encoding='utf-8') as fichier:
+            for index, ligne in enumerate(fichier, start=1):
+                if batabase in ligne:
+                    resultats.append((nom_fichier, index, ligne.strip()))
+        return resultats
+
+    def dossiersDb_recherche(self, dossier_parent, batabase):
+        resultats = []
+        for dossier_racine, _, fichiers in os.walk(dossier_parent):
+            for nom_fichier in fichiers:
+                if nom_fichier.endswith('.txt'):
+                    chemin_fichier = os.path.join(dossier_racine, nom_fichier)
+                    resultats.extend(self.recherche_DB(chemin_fichier, batabase))
+        return resultats
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = RechercheDBApp()
+    ex.show()
+    sys.exit(app.exec_())
